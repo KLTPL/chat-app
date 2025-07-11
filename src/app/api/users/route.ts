86ecb109756b7fcf39d93902867ee03b04c96166
unknown <kltpl@duck.com>
2025-07-11 15:24:17 +0200

    Added prisma, basic schema and basic auth setup

--- src/app/api/users/route.ts
+++ src/app/api/users/route.ts
@@ -1,10 +1,10 @@
-import pool from "@/lib/db";
+import { prisma } from "@/lib/prisma";
 import { NextResponse } from "next/server";
 
 export async function GET() {
   try {
-    const result = await pool.query("SELECT * FROM users");
-    return NextResponse.json(result.rows);
+    const result = await prisma.user.findMany();
+    return NextResponse.json(result);
   } catch (error) {
     console.error("Error", error);
     return NextResponse.json({ error: "Database error" }, { status: 500 });
--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,20 +1,27 @@
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
+import { login } from "@/lib/auth";
+import { redirect } from "next/navigation";
 
 function Login() {
   return (
     <div className="flex items-center justify-center h-[100vh]">
       <form
         className="w-fit flex flex-col gap-3 "
-        action={async data => {
+        action={async formData => {
           "use server";
-          console.log(data);
-          // login
+          await login(formData);
+          redirect("/");
         }}
       >
         <h2 className="text-3xl font-semibold text-center">Login</h2>
-        <Input type="email" placeholder="Email" className="w-3xl" />
-        <Input type="password" placeholder="Password" />
+        <Input
+          name="email"
+          type="email"
+          placeholder="Email"
+          className="w-3xl"
+        />
+        <Input name="password" type="password" placeholder="Password" />
         <Button type="submit">Login</Button>
       </form>
     </div>
